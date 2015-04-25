f5860ff172ac83bf0a1a08b367fcbf2952f67fc2
Doug <djmacdonald@uchicago.edu>
2015-04-25 19:00:20 -0400

    added data folder to dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -375,6 +375,7 @@ module.exports = function (grunt) {
             '.htaccess',
             '*.html',
             'views/{,*/}*.html',
+            'data/{,*/}*.*',
             'images/{,*/}*.{webp}',
             'styles/fonts/{,*/}*.*'
           ]
